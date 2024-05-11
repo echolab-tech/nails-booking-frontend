2cdbeb4c210cc9d6b97a42957640f0be20e80525
huynh.huynh1 <huynh.huynh@rikai.technology>
2024-05-11 16:58:57 +0700

    add new UI customer form

---
+++ src/app/customers/add/page.tsx
@@ -0,0 +1,284 @@
+"use client";
+
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import SelectGroupOne from "@/components/SelectGroup/SelectGroupOne";
+import { Formik, Field, Form, FormikHelpers } from "formik";
+import { CustomerForm } from "@/types/customerForm";
+import flatpickr from "flatpickr";
+import * as Yup from "yup";
+import { useEffect } from "react";
+
+// export const metadata: Metadata = {
+//   title: "Next.js Form Layout | TailAdmin - Next.js Dashboard Template",
+//   description:
+//     "This is Next.js Form Layout page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+// };
+const CustomerNewSchema = Yup.object().shape({
+  lastname: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  firstname: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  phone: Yup.string()
+    .min(10, "Too Short!")
+    .max(15, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
+
+const CustomeNewForm = () => {
+  useEffect(() => {
+    // Init flatpickr
+    flatpickr(".form-datepicker", {
+      mode: "single",
+      static: true,
+      monthSelectorType: "static",
+      dateFormat: "d/m/yy",
+      prevArrow:
+        '<svg className="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M5.4 10.8l1.4-1.4-4-4 4-4L5.4 0 0 5.4z" /></svg>',
+      nextArrow:
+        '<svg className="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M1.4 10.8L0 9.4l4-4-4-4L1.4 0l5.4 5.4z" /></svg>',
+    });
+  }, []);
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Add a new customer" />
+
+      <div className="grid grid-cols-1 gap-9 sm:grid-cols-1">
+        <div className="flex flex-col gap-9">
+          <Formik
+            initialValues={{
+              lastname: "",
+              firstname: "",
+              email: "",
+              source: "",
+              county: "",
+              job: "",
+              language: "",
+              phone: null,
+              pronouns: null,
+              gender: null,
+              birthday: "",
+            }}
+            validationSchema={CustomerNewSchema}
+            onSubmit={(values: CustomerForm) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+              }, 500);
+            }}
+          >
+            {({ errors, touched, validateField, validateForm }) => (
+              <Form>
+                {/* <!-- Profile Form --> */}
+                <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                  <div className="px-6.5 py-4">
+                    <h3 className="font-medium text-black dark:text-white">
+                      Profile
+                    </h3>
+                  </div>
+                  <div className="p-6.5">
+                    <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          First name <span className="text-meta-1">*</span>
+                        </label>
+                        <Field
+                          type="text"
+                          name="firstname"
+                          placeholder="eg.Join"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        />
+                        {errors.firstname && touched.firstname ? (
+                          <div className="text-rose-500">
+                            {errors.firstname}
+                          </div>
+                        ) : null}
+                      </div>
+
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Last name <span className="text-meta-1">*</span>
+                        </label>
+                        <Field
+                          type="text"
+                          name="lastname"
+                          placeholder="eg.Swich"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        />
+                        {errors.lastname && touched.lastname && (
+                          <div className="text-rose-500">{errors.lastname}</div>
+                        )}
+                      </div>
+                    </div>
+                    <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Email <span className="text-meta-1">*</span>
+                        </label>
+                        <Field
+                          type="email"
+                          name="email"
+                          placeholder="example@gmail.com"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        />
+                        {errors.email && touched.email && (
+                          <div className="text-rose-500">{errors.email}</div>
+                        )}
+                      </div>
+
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Phone
+                        </label>
+                        <Field
+                          type="text"
+                          name="phone"
+                          placeholder="+8412121219"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        />
+                        {errors.phone && touched.phone && (
+                          <div className="text-rose-500">{errors.phone}</div>
+                        )}
+                      </div>
+                    </div>
+                    <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Birthday
+                        </label>
+                        <div className="relative">
+                          <input
+                            name="birthday"
+                            className="form-datepicker w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                            placeholder="mm/dd/yyyy"
+                            data-class="flatpickr-right"
+                          />
+
+                          <div className="pointer-events-none absolute inset-0 left-auto right-5 flex items-center">
+                            <svg
+                              width="18"
+                              height="18"
+                              viewBox="0 0 18 18"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M15.7504 2.9812H14.2879V2.36245C14.2879 2.02495 14.0066 1.71558 13.641 1.71558C13.2754 1.71558 12.9941 1.99683 12.9941 2.36245V2.9812H4.97852V2.36245C4.97852 2.02495 4.69727 1.71558 4.33164 1.71558C3.96602 1.71558 3.68477 1.99683 3.68477 2.36245V2.9812H2.25039C1.29414 2.9812 0.478516 3.7687 0.478516 4.75308V14.5406C0.478516 15.4968 1.26602 16.3125 2.25039 16.3125H15.7504C16.7066 16.3125 17.5223 15.525 17.5223 14.5406V4.72495C17.5223 3.7687 16.7066 2.9812 15.7504 2.9812ZM1.77227 8.21245H4.16289V10.9968H1.77227V8.21245ZM5.42852 8.21245H8.38164V10.9968H5.42852V8.21245ZM8.38164 12.2625V15.0187H5.42852V12.2625H8.38164V12.2625ZM9.64727 12.2625H12.6004V15.0187H9.64727V12.2625ZM9.64727 10.9968V8.21245H12.6004V10.9968H9.64727ZM13.8379 8.21245H16.2285V10.9968H13.8379V8.21245ZM2.25039 4.24683H3.71289V4.83745C3.71289 5.17495 3.99414 5.48433 4.35977 5.48433C4.72539 5.48433 5.00664 5.20308 5.00664 4.83745V4.24683H13.0504V4.83745C13.0504 5.17495 13.3316 5.48433 13.6973 5.48433C14.0629 5.48433 14.3441 5.20308 14.3441 4.83745V4.24683H15.7504C16.0316 4.24683 16.2566 4.47183 16.2566 4.75308V6.94683H1.77227V4.75308C1.77227 4.47183 1.96914 4.24683 2.25039 4.24683ZM1.77227 14.5125V12.2343H4.16289V14.9906H2.25039C1.96914 15.0187 1.77227 14.7937 1.77227 14.5125ZM15.7504 15.0187H13.8379V12.2625H16.2285V14.5406C16.2566 14.7937 16.0316 15.0187 15.7504 15.0187Z"
+                                fill="#64748B"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Gender
+                        </label>
+                        <Field
+                          as="select"
+                          name="gender"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        >
+                          <option value="female">Female</option>
+                          <option value="male">Male</option>
+                          <option value="non">Non-binary</option>
+                        </Field>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Additional info */}
+                <div className="mt-6 rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                  <div className="px-6.5 py-4">
+                    <h3 className="font-medium text-black dark:text-white">
+                      Additional info
+                    </h3>
+                  </div>
+                  <div className="p-6.5">
+                    <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Preferred language
+                        </label>
+                        <Field
+                          as="select"
+                          name="language"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        >
+                          <option value="female">Vietnames</option>
+                          <option value="male">Vietnames</option>
+                          <option value="non">Vietnames</option>
+                        </Field>
+                      </div>
+
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Client source
+                        </label>
+                        <Field
+                          as="select"
+                          name="source"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        >
+                          <option value="walk">Walk-in</option>
+                        </Field>
+                      </div>
+                    </div>
+                    <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Occupation
+                        </label>
+                        <Field
+                          type="text"
+                          name="job"
+                          placeholder="Enter customer job"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        />
+                      </div>
+
+                      <div className="w-full xl:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white">
+                          Country
+                        </label>
+                        <Field
+                          as="select"
+                          name="country"
+                          className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        >
+                          <option value="female">Vietnames</option>
+                          <option value="male">Vietnames</option>
+                          <option value="non">Vietnames</option>
+                        </Field>
+                      </div>
+                    </div>
+                    <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+                      <div className="flex w-full justify-end xl:w-1/2">
+                        <button className="justify-center rounded bg-green-600 p-3 font-medium text-gray hover:bg-opacity-90">
+                          Add
+                        </button>
+                      </div>
+                      <div className="w-full xl:w-1/2">
+                        <button className="justify-center rounded bg-slate-900 p-3 font-medium text-gray hover:bg-opacity-90">
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default CustomeNewForm;
---
+++ src/types/customerForm.ts
@@ -0,0 +1,13 @@
+export type CustomerForm = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: number | null;
+  birthday: string | null;
+  gender: number | null;
+  pronouns: number | null;
+  language: string;
+  source: string;
+  job: string;
+  county: string;
+};
