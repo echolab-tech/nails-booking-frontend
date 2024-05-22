6a0a4579cea39cbaed4b06609d04579a0e77e8ef
vominhnguyet2202 <vominhnguyet1801@gmail.com>
2024-05-22 11:56:26 +0700

    fix: update table

--- src/app/scheduled/page.tsx
+++ src/app/scheduled/page.tsx
@@ -109,68 +109,74 @@ const ScheduledPage = () => {
     <DefaultLayout>
       <Breadcrumb pageName="Scheduled shifts" />
 
-      <div className="h-[500px] rounded-2xl border-2 border-stroke  pb-2.5 pt-6  dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-        <div className="flex flex-col">
-          <div
-            className={`grid grid-cols-3 rounded-sm  pl-5 pr-5 sm:grid-cols-7`}
-          >
-            {HEADERSTABLE.map((header) => (
-              <div key={header} className="pl-2.5">
-                <h5 className="text-lg font-semibold text-black">{header}</h5>
-              </div>
-            ))}
-          </div>
-          {SCHEDULEDSHIFTS.map((item, index) => (
-            <div
-              key={index}
-              className={`grid grid-cols-3 rounded-sm p-5 sm:grid-cols-7`}
-            >
-              <div className="flex items-center gap-4">
-                <Image
-                  src={item.avata}
-                  alt={item.userName}
-                  width={40}
-                  height={40}
-                />
+      <div className="h-[500px] w-full rounded-2xl border-2  border-stroke pb-2.5  pt-6 dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="text-gray-500 dark:text-gray-400 w-full text-left text-sm rtl:text-right">
+            <thead>
+              <tr>
+                {HEADERSTABLE.map((header) => (
+                  <th scope="row" key={header} className="pl-10 pr-10">
+                    <h5 className="text-lg font-semibold text-black">
+                      {header}
+                    </h5>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {SCHEDULEDSHIFTS.map((item, index) => (
+                <tr
+                  key={index}
+                  className=" even:bg-gray-50 even:dark:bg-gray-800"
+                >
+                  <td scope="row" className="px-6 py-4">
+                    <div className="flex w-max items-center gap-2">
+                      <Image
+                        src={item.avata}
+                        alt={item.userName}
+                        width={40}
+                        height={40}
+                        className="w-[40px' h-[40px]"
+                      />
 
-                <div>
-                  <p className="text hidden font-medium text-black dark:text-white sm:block">
-                    {item.userName}
-                  </p>
-                  <p className="hidden text-sm text-black dark:text-white sm:block">
-                    {item.totalHours}
-                  </p>
-                </div>
-                <Image
-                  src="/images/scheduled/edit.svg"
-                  width={50}
-                  height={50}
-                  alt="edit"
-                />
-              </div>
+                      <div className="">
+                        <p className="flex text-lg font-medium text-black dark:text-white ">
+                          {item.userName}
+                        </p>
+                        <p className="w-max text-sm text-black dark:text-white ">
+                          {item.totalHours}
+                        </p>
+                      </div>
+                      <Image
+                        src="/images/scheduled/edit.svg"
+                        width={50}
+                        height={50}
+                        alt="edit"
+                      />
+                    </div>
+                  </td>
 
-              {item.scheduleds.map((scheduled, index) => (
-                <>
-                  <div
-                    className="bg-gray-4 flex  max-w-max items-center rounded-2xl pb-1.5 pl-5 pr-5 pt-1.5"
-                    key={index}
-                  >
-                    <p className="text-lg text-black dark:text-white sm:block">
-                      {scheduled.time}
-                    </p>
-                  </div>
-                </>
+                  {item.scheduleds.map((scheduled, index) => (
+                    <td scope="row" className="px-6 py-4" key={index}>
+                      <div className="bg-gray-4 flex w-max items-center rounded-2xl pb-1.5 pl-1.5 pr-1.5 pt-1.5 lg:pl-5 lg:pr-5">
+                        <p className="whitespace-nowrap text-lg text-black dark:text-white sm:block">
+                          {scheduled.time}
+                        </p>
+                      </div>
+                    </td>
+                  ))}
+                  <td scope="row" className="px-6 py-4">
+                    <Image
+                      src="/images/scheduled/plus.svg"
+                      width={40}
+                      height={40}
+                      alt="edit"
+                    />
+                  </td>
+                </tr>
               ))}
-              <div>
-                <Image
-                  src="/images/scheduled/plus.svg"
-                  width={50}
-                  height={50}
-                  alt="edit"
-                />
-              </div>
-            </div>
-          ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </DefaultLayout>
