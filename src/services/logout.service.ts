e752c7e542b8fac54403c8394ec148eaef5b7b42
vominhnguyet2202 <vominhnguyet1801@gmail.com>
2024-07-06 16:52:37 +0700

    feat: rap api logout

---
+++ src/services/logout.service.ts
@@ -0,0 +1,12 @@
+
+import { http } from "../lib/http";
+
+
+ export const logout = async (): Promise<any> => {
+    try {
+      const response = await http.post<any>("/logout");
+      return response.data;
+    } catch (error) {
+      throw new Error(`Logout failed: ${error}`);
+    }
+  };
