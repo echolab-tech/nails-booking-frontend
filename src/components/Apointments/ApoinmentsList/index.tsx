7f11f875c0c84942a61a79f1d2346baa88f3ffb2
trinhvany <trinhvany201@gmail.com>
2025-08-29 21:51:23 +0700

    handle print and delete appointment

--- src/components/Apointments/ApoinmentsList/index.tsx
+++ src/components/Apointments/ApoinmentsList/index.tsx
@@ -109,7 +109,7 @@ const ApointmentList = () => {
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="p-6.5">
         <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
-          <div className="w-full xl:w-1/3">
+          <div className="w-full xl:w-1/4">
             {/* <label className="mb-3 block text-sm font-medium text-black dark:text-white">
                 Name <span className="text-meta-1">*</span>
               </label> */}
@@ -122,7 +122,7 @@ const ApointmentList = () => {
               className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
             />
           </div>
-          <div className="w-full xl:w-1/3">
+          <div className="w-full xl:w-2/4">
             <div className="mb-4.5 flex flex-col gap-4 xl:flex-row xl:items-center">
               <div className="w-full flex-1 xl:w-auto">
                 <input
@@ -149,8 +149,8 @@ const ApointmentList = () => {
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-1/3">
-            <div className="flex w-full justify-center gap-2">
+          <div className="w-full xl:w-1/4">
+            <div className="flex w-full justify-center gap-2" style={{ marginTop: "5px" }}>
               <button
                 className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
                 onClick={handleSearch}
@@ -286,7 +286,7 @@ const ApointmentList = () => {
       />
             <DialogConfirm
         openModal={openModalConfirm}
-        message="Do you want to print appointments from the screen??"
+        message="Do you want to print appointments from the screen?"
         onClose={closeModalConfirm}
       >
         <button
